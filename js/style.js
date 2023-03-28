3293a18e82cb24158561689ad1c3bed7fe56d0f9
Dnaux-dev <email@youremail.com>
2023-03-28 00:01:42 -0700

    first commit

---
+++ js/style.js
@@ -0,0 +1,110 @@
+let slider = tns({
+  container : ".my-slider",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls',
+  prevButton : ".previous",
+  nextButton : ".next",
+  responsive: {
+    1600 : {
+      items : 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480:{
+      items: 1
+    }
+  }
+})
+
+
+let slider1 = tns({
+  container : ".my-slider1",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls1',
+  prevButton : ".previous1",
+  nextButton : ".next1",
+  responsive: {
+    1600 : {
+      items : 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480:{
+      items: 1
+    }
+  }
+})
+
+
+let slider2 = tns({
+  container : ".my-slider2",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls2',
+  prevButton : ".previous2",
+  nextButton : ".next2",
+  responsive: {
+    1600 : {
+      items : 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480:{
+      items: 1
+    }
+  }
+})
+
+
+let slider3 = tns({
+  container : ".my-slider3",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls3',
+  prevButton : ".previous3",
+  nextButton : ".next3",
+  responsive: {
+    1600 : {
+      items : 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480:{
+      items: 1
+    }
+  }
+})
